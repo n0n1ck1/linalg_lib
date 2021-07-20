4d6527c58e661e58b234f3f9cfc3b6afab38a074
n0n1ck <star_tyom@mail.ru>
2021-07-20 18:42:50 +0300

    bad alloc

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -5,6 +5,7 @@
 #include<atomic>
 #include<iostream>
 #include<utility>
+#include<exception>
 
 template <typename T>
 class Matrix {
@@ -44,16 +45,26 @@ public:
   size_t GetLength() const {
     return length_;
   }
-  std::pair<size_t, size_t> size() const {
-    std::pair<size_t, size_t> res = std::make_pair(length_, width_);
-    return res;
-  }
   std::vector<T> operator[](const size_t& row) const {
     return matrix_[row];
   }
   std::vector<T>& operator[](const size_t& row) {
     return matrix_[row];
   }
+  std::vector<T> GetColumn(const size_t& number) const {
+    if (number > width_) {
+      throw "Out of bounds";
+      std::vector<T> blank;
+      return blank;
+    }
+    std::vector<T> res;
+    res.reserve(length_);
+    for (size_t i = 0; i < length_; ++i) {
+      res.push_back(matrix_[i][number]);
+    }
+    return res;
+  }
+
   Matrix& operator=(const Matrix& other) {
     matrix_ = other.matrix_;
     width_ = other.width_;
@@ -91,16 +102,46 @@ private:
 
 template<typename T>
 std::ostream& operator<<(std::ostream& out, const Matrix<T>& matrix) {
-    for (size_t i = 0; i < matrix.size().first; ++i) {
-        for (size_t j = 0; j < matrix.size().second; ++j) {
+    for (size_t i = 0; i < matrix.GetLength(); ++i) {
+        for (size_t j = 0; j < matrix.GetWidth(); ++j) {
             if (j != 0) {
                 out << "\t";
             }
             out << matrix[i][j];
         }
-        if (i != matrix.size().first - 1) {
-            out << "\n";
-        }
+        out << "\n";
     }
     return out;
+}
+
+template<typename T>
+T MultiplyVectors(std::vector<T> row, std::vector<T> column) {
+  T res;
+  res = static_cast<T>(0);
+  for (size_t i = 0; i < row.size(); ++i) {
+    res += row[i] * column[i];
+  }
+  return res;
+}
+
+template<typename T>
+Matrix<T> operator*(Matrix<T> left, Matrix<T> right) {
+  if (left.GetWidth() != right.GetLength()) {
+    throw "Incompatible matrices";
+    Matrix<T> blank(0, 0);
+    return blank;
+  }
+  Matrix<T> res(left.GetLength(), right.GetWidth());
+  std::vector<std::thread> threads;
+  for (size_t i = 0; i < left.GetLength(); ++i) {
+    for (size_t j = 0; j < right.GetWidth(); ++j) {
+      threads.emplace_back([&] {
+        res[i][j] = MultiplyVectors(left[i], right.GetColumn(j));
+      });
+    }
+  }
+  for (auto& t : threads) {
+    t.join();
+  }
+  return res;
 }
--- matrix/test.cpp
+++ matrix/test.cpp
@@ -1,7 +1,6 @@
 #include "timeout_guard.h"
 #include <gtest/gtest.h>
 
-
 #include "matrix.h"
 
 TEST(Matrix, Generation) {
@@ -14,4 +13,18 @@ TEST(Matrix, Generation) {
     }
     Matrix<int> matrix_2(v);
     ASSERT_EQ(matrix_1, matrix_2);
+}
+
+TEST(Matrix, SimpleMultiplication) {
+  std::vector<std::vector<int>> left = {{1, 2, 3}, {4, 5, 6}};
+  Matrix<int> matrix_left(left);
+  std::vector<std::vector<int>> right = {{2, 3, 4, 5}, {4, 3, 2, 1}};
+  right.push_back({4,5,6,7});
+  Matrix<int> matrix_right(right);
+  std::vector<std::vector<int>> expected = {{22, 24, 26, 28}, {44, 49, 54, 59}};
+  //std::cout<< matrix_left << "\n"<< matrix_right;
+  Matrix<int> matrix_expected(expected);
+//  std::cout <<"\n"<< matrix_expected <<"\n";
+  //std::cout << matrix_left * matrix_right <<"\n";
+  ASSERT_EQ(matrix_left * matrix_right, matrix_expected);
 }
