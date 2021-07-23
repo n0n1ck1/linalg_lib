5be8f9cb1c8639054831a5c680d2e6cd699fd5a8
n0n1ck1 <51696616+n0n1ck1@users.noreply.github.com>
2021-07-23 13:02:50 +0300

    made matrix_ 1-dimentional (#8)
    made matrix one-dimentional + cmake changes

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -2,105 +2,88 @@
 
 #include<vector>
 #include<thread>
-#include<atomic>
 #include<iostream>
-#include<utility>
+#include<exception>
+//#include<barrier>
 
 template <typename T>
 class Matrix {
 public:
-  Matrix (const std::vector<std::vector<T>>& matrix) {
-    matrix_ = matrix;
-    width_ = matrix[0].size();
-    length_ = matrix.size();
-  }
-  Matrix (const size_t& h, const size_t& w) {
-    length_ = h;
-    width_ = w;
-    std::vector<std::thread> threads;
-    for (size_t i = 0; i < h; ++i) {
-      threads.emplace_back([&]{
-        std::vector<T> blank_row(w);
-        matrix_.push_back(blank_row);
-      });
+    Matrix (const std::vector<std::vector<T>>& matrix) {
+        width_ = matrix[0].size();
+        length_ = matrix.size();
+        for (const auto& row : matrix) {
+            for (const auto& x : row) {
+                matrix_.push_back(x);
+            }
+        }
     }
-    for (auto& t : threads) {
-      t.join();
+    Matrix (const size_t& h, const size_t& w) {
+        std::vector<T> v(h * w);
+        matrix_ = v;
+        length_ = h;
+        width_ = w;
     }
-  }
-  Matrix (const Matrix& other) {
-    matrix_ = other.matrix_;
-    width_ = other.width_;
-    length_ = other.length_;
-  }
-  Matrix (const Matrix&& other) {
-    matrix_ = other.matrix_;
-    width_ = other.width_;
-    length_ = other.length_;
-  }
-  size_t GetWidth() const {
-    return width_;
-  }
-  size_t GetLength() const {
-    return length_;
-  }
-  std::pair<size_t, size_t> size() const {
-    std::pair<size_t, size_t> res = std::make_pair(length_, width_);
-    return res;
-  }
-  std::vector<T> operator[](const size_t& row) const {
-    return matrix_[row];
-  }
-  std::vector<T>& operator[](const size_t& row) {
-    return matrix_[row];
-  }
-  Matrix& operator=(const Matrix& other) {
-    matrix_ = other.matrix_;
-    width_ = other.width_;
-    length_ = other.length_;
-    return this;
-  }
-  Matrix& operator=(const Matrix&& other) {
-    matrix_ = other.matrix_;
-    width_ = other.width_;
-    length_ = other.length_;
-    return this;
-  }
-  bool operator==(const Matrix& other) const {
-    return matrix_ == other.matrix_;
-  }
-  Matrix& operator+(const Matrix& other) {
-    std::vector<std::thread> threads;
-    Matrix res(length_, width_);
-    for (size_t i = 0; i < length_; ++i) {
-      for (size_t j = 0; j < width_; ++j) {
-        threads.emplace_back([&]{
-          res[i][j] = matrix_[i][j] + other[i][j];
-        });
-      }
+    Matrix (const Matrix& other) {
+        matrix_ = other.matrix_;
+        width_ = other.width_;
+        length_ = other.length_;
     }
-    for (auto& t : threads) {
-      t.join();
+    Matrix (const Matrix&& other) {
+        matrix_ = other.matrix_;
+        width_ = other.width_;
+        length_ = other.length_;
     }
-  }
+    size_t GetWidth() const {
+        return width_;
+    }
+    size_t GetLength() const {
+        return length_;
+    }
+    T operator()(const size_t& row, const size_t& column) const {
+        return matrix_[width_ * row + column];
+    }
+    T& operator()(const size_t& row, const size_t& column) {
+        return matrix_[width_ * row + column];
+    }
+    Matrix& operator=(const Matrix& other) {
+        matrix_ = other.matrix_;
+        width_ = other.width_;
+        length_ = other.length_;
+        return *this;
+    }
+    Matrix& operator=(Matrix&& other) noexcept {
+        matrix_ = other.matrix_;
+        width_ = other.width_;
+        length_ = other.length_;
+        return *this;
+    }
+    bool operator==(const Matrix& other) const {
+        return matrix_ == other.matrix_;
+    }
+    Matrix operator+(const Matrix& other) {
+        Matrix res(length_, width_);
+        for (size_t i = 0; i < width_ * length_; ++i) {
+            res.matrix_[i] = matrix_[i] + other.matrix_[i];
+        }
+        return res;
+    }
 private:
-  std::vector<std::vector<T>> matrix_;
-  size_t width_;
-  size_t length_;
+    std::vector<T> matrix_;
+    size_t width_;
+    size_t length_;
 };
 
 template<typename T>
 std::ostream& operator<<(std::ostream& out, const Matrix<T>& matrix) {
-    for (size_t i = 0; i < matrix.size().first; ++i) {
-        for (size_t j = 0; j < matrix.size().second; ++j) {
+    for (size_t i = 0; i < matrix.GetLength(); ++i) {
+        for (size_t j = 0; j < matrix.GetWidth(); ++j) {
             if (j != 0) {
                 out << "\t";
             }
-            out << matrix[i][j];
+            out << matrix(i, j);
         }
-        if (i != matrix.size().first - 1) {
-            out << "\n";
-        }
+        out << "\n";
     }
     return out;
 }
--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -1,8 +1,7 @@
-#include "timeout_guard.h"
+#include "util/timeout_guard.h"
 #include <gtest/gtest.h>
 
-
-#include "matrix.h"
+#include "../matrix/matrix.h"
 
 TEST(Matrix, Generation) {
     TimeoutGuard guard(1s);
@@ -14,4 +13,11 @@ TEST(Matrix, Generation) {
     }
     Matrix<int> matrix_2(v);
     ASSERT_EQ(matrix_1, matrix_2);
+}
+
+TEST(Matrix, Addition) {
+  Matrix<int> matrix_1({{1, 2, 3}, {3, 2, 1}});
+  Matrix<int> matrix_2({{3, 2, 1}, {1, 2, 3}});
+  Matrix<int> expected({{4, 4, 4}, {4, 4, 4}});
+  ASSERT_EQ(matrix_1 + matrix_2, expected);
 }
