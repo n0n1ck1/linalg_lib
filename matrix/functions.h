667940c6a567c5621fbbcfbdf46974304beb042e
n0n1ck <star_tyom@mail.ru>
2021-07-26 23:10:16 +0300

    made emplace_backs prettier

--- matrix/functions.h
+++ matrix/functions.h
@@ -232,16 +232,16 @@ T det(Matrix<T> matrix) {
             bool has_non_zero = false;
             size_t index_non_zero;
             for (size_t k = 0; k < n_threads; ++k) {
-                threads.emplace_back([k, &matrix, &has_non_zero, &index_non_zero, &i, &width, &n_threads]{
+                threads.emplace_back([&](const size_t& id){
                     // divide the rows in range [i, width] evenly between all threads
-                    for (size_t j = i + k * (width - i ) / n_threads; j < i + (k + 1) * (width - i) / n_threads; ++j) {
+                    for (size_t j = i + id * (width - i ) / n_threads; j < i + (id + 1) * (width - i) / n_threads; ++j) {
                         if (matrix(j, i) != 0) {
                             has_non_zero = true;
                             index_non_zero = j;
                             return;
                         }
                     }
-                });
+                }, k);
             }
             for (auto& t : threads) {
                 t.join();
@@ -254,17 +254,17 @@ T det(Matrix<T> matrix) {
         }
         threads.clear();
         // the same as above, except the first thread mustn't have the i-th row in it
-        threads.emplace_back([&matrix, &width, &n_threads, &i] {
+        threads.emplace_back([&] {
             for (size_t j = i + 1; j < i + (width - i) / n_threads; ++j) {
                 matrix.row_addition(j, i, static_cast<T>(-1) * matrix(j, i) / matrix(i, i));
             }
         });
         for (size_t k = 1; k < n_threads; ++k) {
-            threads.emplace_back([k, &matrix, &width, &n_threads, &i] {
-                for (size_t j = std::max(i + k * (width - i) / n_threads, i + 1); j < i + (k + 1) * (width - i) / n_threads; ++j) {
+            threads.emplace_back([&](const size_t& id) {
+                for (size_t j = std::max(i + id * (width - i) / n_threads, i + 1); j < i + (id + 1) * (width - i) / n_threads; ++j) {
                     matrix.row_addition(j, i, static_cast<T>(-1) * matrix(j, i) / matrix(i, i));
                 }
-            });
+            }, k);
         }
         for (auto& t : threads) {
             t.join();
